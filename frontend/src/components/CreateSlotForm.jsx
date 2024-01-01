b42e06a513cdfb9c97c311f5f8a9773a2be57291
khalilahmad3212 <arzishjani1613@gmail.com>
2024-01-01 10:56:10 +0500

    frontend:appointments list done

--- frontend/src/components/CreateSlotForm.jsx
+++ frontend/src/components/CreateSlotForm.jsx
@@ -3,82 +3,88 @@ import { Formik } from 'formik';
 import MyTimePicker from './commons/MyTimePicker';
 import { Button } from '@mui/material';
 import api from '../utils/api'
+import { useNavigate } from 'react-router-dom';
 
-const CreateSlotForm = () => (
+const CreateSlotForm = () => {
 
-  <Formik
-    initialValues={{ startTime: '', endTime: '' }}
-    // validate={values => {
-    //   const errors = {};
-    //   if (!values.email) {
-    //     errors.email = 'Required';
-    //   } else if (
-    //     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-    //   ) {
-    //     errors.email = 'Invalid email address';
-    //   }
-    //   return errors;
-    // }}
-    onSubmit={(values, { setSubmitting }) => {
-      api.post("/appointments", values)
-        .then((appointment) => {
-          setSubmitting(false);
-          console.log('Res: ', appointment);
-        }).catch((err) => {
-          setSubmitting(false);
-          console.log(err)
-        })
-    }}
-  >
-    {({
-      values,
-      errors,
-      touched,
-      handleChange,
-      handleBlur,
-      handleSubmit,
-      isSubmitting,
-      setFieldValue
-      /* and other goodies */
-    }) => (
-      <form onSubmit={handleSubmit}>
-        {/* <input
+  const navigate = useNavigate();
+
+  return (
+    <Formik
+      initialValues={{ startTime: '', endTime: '' }}
+      // validate={values => {
+      //   const errors = {};
+      //   if (!values.email) {
+      //     errors.email = 'Required';
+      //   } else if (
+      //     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+      //   ) {
+      //     errors.email = 'Invalid email address';
+      //   }
+      //   return errors;
+      // }}
+      onSubmit={(values, { setSubmitting }) => {
+        api.post("/slots", values)
+          .then((appointment) => {
+            console.log('Res: ', appointment);
+            setSubmitting(false);
+            navigate("/slots")
+          }).catch((err) => {
+            setSubmitting(false);
+            console.log(err)
+          })
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        setFieldValue
+        /* and other goodies */
+      }) => (
+        <form onSubmit={handleSubmit}>
+          {/* <input
           type="datetime-local"
           name="email"
           onChange={handleChange}
           onBlur={handleBlur}
           value={values.email}
         /> */}
-        <div>
-          <MyTimePicker
-            label={"Start Time"}
-            setFieldValue={setFieldValue}
-            fieldName={"startTime"}
-          />
-          <MyTimePicker
-            label={"End Time"}
-            setFieldValue={setFieldValue}
-            fieldName={"endTime"}
-          />
-        </div>
-        {/* {errors.email && touched.email && errors.email} */}
-        {/* <input
+          <div>
+            <MyTimePicker
+              label={"Start Time"}
+              setFieldValue={setFieldValue}
+              fieldName={"startTime"}
+            />
+            <MyTimePicker
+              label={"End Time"}
+              setFieldValue={setFieldValue}
+              fieldName={"endTime"}
+            />
+          </div>
+          {/* {errors.email && touched.email && errors.email} */}
+          {/* <input
           type="password"
           name="password"
           onChange={handleChange}
           onBlur={handleBlur}
           value={values.password}
         /> */}
-        {/* {errors.password && touched.password && errors.password} */}
-        {/* <button type="submit" disabled={isSubmitting}>
+          {/* {errors.password && touched.password && errors.password} */}
+          {/* <button type="submit" disabled={isSubmitting}>
           Submit
         </button> */}
-        <Button variant="contained" type="submit" disabled={isSubmitting}>
-          Create
-        </Button>
-      </form>
-    )}
-  </Formik>
-);
+          <Button variant="contained" type="submit" disabled={isSubmitting}>
+            Create
+          </Button>
+        </form>
+      )}
+    </Formik>
+  )
+};
 
 export default CreateSlotForm
--- frontend/src/components/ListAppointments.jsx
+++ frontend/src/components/ListAppointments.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import api from '../utils/api';
 
 const ListAppointments = () => {
+
+  const [appointments, setAppointments] = useState([]);
+
+  const getAppointments = () => {
+    api.get("/appointments")
+      .then(({ data }) => {
+        console.log('Res: ', data);
+        setAppointments(data);
+      }).catch((err) => {
+        setSubmitting(false);
+        console.log(err)
+      })
+  }
+
+  useEffect(() => {
+    getAppointments();
+  }, [])
+
   return (
-    <div>ListAppointments</div>
+    <div>
+      <h1>Booked Appointments</h1>
+      <div>
+        {
+          appointments?.map((appointment) => (
+            <div key={appointment._id}>
+              <h3>{appointment.name}</h3>
+              <h3>{appointment.email}</h3>
+              <p>{appointment.slot.startTime + appointment.slot.startTime} </p>
+            </div>
+          ))
+        }
+      </div>
+    </div>
   )
 }
 
